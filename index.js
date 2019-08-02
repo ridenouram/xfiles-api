08130b0f02c7228b6acfa4b6218055586c7cadc4
Anna Ridenour <annamridenour@gmail.com>
2019-08-02 14:31:05 -0700

    scraping list of monster-of-week names

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const request = require('superagent');
+const cheerio = require('cheerio');
 const { parse } = require('node-html-parser');
 
+// request({
+//   method: 'GET',
+//   url: 'https://x-files.fandom.com/wiki/Monster_of_the_Week',
+// }, (err, res, body) => {
+//   console.log(body);
+//   if(err) return console.error(err);
+//   let $ = cheerio.load(body);
+  
+//   // let character = $('<h2 class="pi-item pi-item-spacing pi-title" data-source="Title">Audrey Horne</h2>');
+//   // console.log(character.text());
+// });
+
 const scrape = () => {
-  return request.get(`https://x-files.fandom.com/wiki/Monster_of_the_Week`)
+  return request.get(`https://x-files.fandom.com/wiki/Category:Monster_of_the_Week#`)
     .then(res => res.text)
     .then(parse)
-    .then(html => html.querySelector('tr'))
-    .then(child => console.log(child.toString()));
-    // .then(findCharLink)
-    // .then(names => console.log(names.childNodes));
-    // .then(findCharNames);
+    .then(findCharLink)
+    .then(findCharNames)
+    .then(names => console.log(names));
+    // // .then(tr => tr.querySelector('a'))
+    // .then(stuff => console.log(stuff));
 };
 
-const findCharLink = html => html.querySelectorAll('b');
+const findCharLink = html => html.querySelectorAll('.category-page__member-link');
 const findCharNames = objs => {
   const names = objs.map(obj => obj.childNodes[0].rawText);
   return names.filter(name => !name.includes('Category:'));
 };
+
+// const html = scrape();
+// const $ = cheerio.load(html);
+// console.log($("a"));
+
+
+// const findCharLink = html => html.querySelectorAll('b');
+// const findCharNames = objs => {
+//   const names = objs.map(obj => obj.childNodes[0].rawText);
+//   return names.filter(name => !name.includes('Category:'));
+// };
 
 scrape();
