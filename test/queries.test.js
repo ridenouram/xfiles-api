99acfaf721aeef2915ba86810811210e7068df7b
Anna Ridenour <annamridenour@gmail.com>
2019-10-07 09:27:41 -0700

    updating tests

--- test/queries.test.js
+++ test/queries.test.js
@@ -1,55 +1,67 @@
-const { pool } = require('../lib/utils/connect');
 const app = require('../lib/app');
 const request = require('supertest');
+const { pool } = require('../lib/utils/connect');
+
+beforeAll(() => {
+  pool.connect();
+});
 
 afterAll(() => {
-  pool.end().then(() => console.log('pool has ended'));
+  pool.end();
 });
 
 describe('routes', () => {
   it('gets all characters', () => {
-    pool.connect();
     return request(app)
       .get('/api/v1/characters')
       .then(res => {
-        expect(res.body).toHaveLength(20);
+        expect(res.body.results).toHaveLength(461);
       });
   });
 
   it('gets all characters with variable limit', () => {
-    pool.connect();
     return request(app)
       .get('/api/v1/characters?perPage=30')
       .then(res => {
-        expect(res.body).toHaveLength(30);
+        expect(res.body.results).toHaveLength(30);
+        expect(res.body.quantity).toBe(30);
       });
   });
 
-  it('gets character by id', () => {
-    pool.connect();
+  it('gets all characters with paging', () => {
+    return request(app) 
+      .get('/api/v1/characters?page=2&perPage=20')
+      .then(res => {
+        expect(res.body.results).toHaveLength(20);
+        expect(res.body.results[0].name).toBe('Anita Budahas');
+      });
+  });
+
+  it('gets characters by category, and Main characters was successfully added as a category', () => {
     return request(app)
-      .get('/api/v1/characters/207')
+      .get('/api/v1/characters?category=Main_characters')
+      .then(res => {
+        expect(res.body.quantity).toBe(14);
+        expect(res.body.results[0].name).toBe('Alex Krycek');
+      });
+  });
+
+  it('gets character by name', () => {
+    return request(app)
+      .get('/api/v1/characters/Dana_Scully')
       .then(res => {
         expect(res.body).toHaveLength(1);
+        expect(res.body[0].name).toBe('Dana Scully');
       });
   });
 
-  it('gets characters by category', () => {
-    pool.connect();
-    return request(app)
-      .get('/api/v1/characters?category=Criminals')
-      .then(res => {
-        expect(res.body).toHaveLength(20);
-      });
-  });
 
   it('gets all characters with variable limit, page, and category', () => {
-    pool.connect();
     return request(app)
       .get('/api/v1/characters?category=Criminals&page=2&perPage=5')
       .then(res => {
-        expect(res.body).toHaveLength(5);
-        expect(res.body[0].id).toEqual(261);
+        expect(res.body.quantity).toBe(5);
+        expect(res.body.results[0].name).toBe('Augustine O\'Fallon');
       });
   });
 });
